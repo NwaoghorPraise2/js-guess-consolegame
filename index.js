801f6b4cff21c618b354bc8a0b8a28fad54d62f1
NwaoghorPraise2 <chukunwaoghor@gmail.com>
2022-10-21 16:25:48 +0000

    working1

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ let rangeMaxValue = 2;
 let checkCondition = true;
 let userGuess = 0;
 
+//Reusegable Functions
 let tryAgain = () => {
     console.log(`oops...,${username} you guessed wrong. \n Try Again`);
     userGuess = parseInt(`Pick a number between 1 and 
@@ -27,9 +28,9 @@ let restart = () => {
 const welcomeMessage = "Welcome to Js Guessing Game. \n By Nwaoghor Praise for I4gxZuri Traning Cohort II 2022.\n I need you to follow the instuctions, thank you.";
 console.log(welcomeMessage);
 let username = prompt("Enter your name");
+
 while (checkCondition) {
   if (username) {
-// console.log(username);
     let randomNumber = Math.floor(Math.random() * rangeMaxValue + 1)
     console.log(`${username}, I have pick a number between 1 and ${rangeMaxValue}`);
     userGuess = parseInt(prompt(`${username}, Try guessing this number`));
@@ -46,64 +47,10 @@ while (checkCondition) {
     } else if (rangeMaxValue === maxLevel) {
       console.log (`${username}, Congratulations... You nailed it`);
       console.clear();
-     // checkCondition = false;
       restart();
-      // console.clear();
-      // checkCondition = false;
     } else {
       tryAgain();
     } 
-    // if (newNumber == NaN) {
-    // console.log(`${Username}, you enter an alphabet. Please enter a number`)
-
-    // }
-
   }
 }
-
-// console.log(`${username} Lets get the game started`);
-
-// tryAgain();
-// let predictedNumber = parseInt(prompt('Enter a number')) ;
-// let randomNumber = Math.floor(Math.random() * maxNumber) + 1;
-// game();
-
-// function game () {
-
-// if(predictedNumber === randomNumber){
-// //    stage =  stage + ;
-//     // point ++;
-//     // increasePointStage();
-//     console.log(point);
-//     // console.log(`Hurry 1 mark Earde Move to Stage 2 ${point} ${stage}`);
-//     increase();
-// } 
-
-
-
-
-
-
-
-
-// increase();
-
-
-// let points = 0;
-// let level = 1;
-// let maxlevel = 10;
-
-// for (let i = 2; i < maxlevel; i++) {
-//   let randomNumber = Math.floor(Math.random() * i) + 1;
-//   // console.log(randomNumber)
-//   let userGuess = parseInt(prompt(`Enter Number between 1 and ${i}`))
-
-//   if (userGuess === NaN) {
-//     let userGuess = parseInt(prompt(`Enter Number between`))
-
-//   }
-
-//   // console.log(randomNumber);
-//   console.log(userGuess)
-// }
 
